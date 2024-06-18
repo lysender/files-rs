7a598a6894272d289e0f593f752ece638c924be3
Lysender <lysender@darkstar-win>
2024-06-18 13:54:47 +0800

    fix: use json helpers

--- src/web/home.rs
+++ src/web/home.rs
@@ -1,4 +1,4 @@
-use axum::{body::Body, extract::State, http::StatusCode, response::Response};
+use axum::{extract::State, response::IntoResponse, Json};
 use serde::Serialize;
 
 use super::server::AppState;
@@ -9,8 +9,9 @@ pub struct AppMeta {
     pub version: String,
 }
 
-pub async fn home_handler(State(_state): State<AppState>) -> Response<Body> {
-    let r = Response::builder().status((StatusCode::OK).as_u16());
-    let body = "Home page".to_string();
-    r.body(Body::from(body)).unwrap()
+pub async fn home_handler(State(_state): State<AppState>) -> impl IntoResponse {
+    Json(AppMeta {
+        name: "files-rs".to_string(),
+        version: "0.1.0".to_string(),
+    })
 }
