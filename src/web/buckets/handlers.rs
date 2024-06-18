e1cd9d357bccae8971c5793505ed340ea8dd34db
Lysender <lysender@darkstar-win>
2024-06-18 14:13:13 +0800

    fix: list buckets json response

--- src/web/buckets/handlers.rs
+++ src/web/buckets/handlers.rs
@@ -1,11 +1,40 @@
-use axum::{body::Body, extract::State, http::StatusCode, response::Response};
+use axum::{
+    body::Body,
+    extract::State,
+    http::StatusCode,
+    response::{IntoResponse, Response},
+    Json,
+};
 
-use crate::web::server::AppState;
+use crate::{
+    files::queries::buckets::list_buckets,
+    web::{error::ErrorResponse, server::AppState},
+};
 
-pub async fn list_buckets_handler(State(_state): State<AppState>) -> Response<Body> {
-    let r = Response::builder().status((StatusCode::OK).as_u16());
-    let body = "Home page".to_string();
-    r.body(Body::from(body)).unwrap()
+pub async fn list_buckets_handler(State(state): State<AppState>) -> Response<Body> {
+    let pool = state.db_pool.clone();
+    let config = state.config.clone();
+    let client_id = config.client_id.clone();
+
+    let buckets_res = list_buckets(pool, client_id.as_str()).await;
+    let Ok(buckets) = buckets_res else {
+        let error_body = ErrorResponse {
+            status_code: StatusCode::INTERNAL_SERVER_ERROR.as_u16(),
+            message: "Failed to list buckets".to_string(),
+            error: "Internal Server Error".to_string(),
+        };
+        let error_res = Response::builder()
+            .status(StatusCode::INTERNAL_SERVER_ERROR)
+            .body(Body::from(serde_json::to_string(&error_body).unwrap()))
+            .unwrap();
+        return error_res;
+    };
+
+    let res = Response::builder()
+        .status(StatusCode::OK)
+        .body(Body::from(serde_json::to_string(&buckets).unwrap()))
+        .unwrap();
+    res
 }
 
 pub async fn create_bucket_handler(State(_state): State<AppState>) -> Response<Body> {
