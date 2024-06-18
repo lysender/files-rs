9016002acc796226ae397a83e1eceb7971c43949
Lysender <lysender@darkstar-win>
2024-06-18 08:38:17 +0800

    fix: add test for id generator

--- src/uuid.rs
+++ src/uuid.rs
@@ -3,3 +3,21 @@ use uuid::Uuid;
 pub fn generate_id() -> String {
     Uuid::now_v7().as_simple().to_string()
 }
+
+#[cfg(test)]
+mod tests {
+    use std::str::FromStr;
+
+    use super::*;
+
+    #[test]
+    fn test_generate_id() {
+        // Should be a 32-character uuid string
+        let id = generate_id();
+        assert_eq!(id.len(), 32);
+
+        // Can be parsed back as uuid
+        let parsed = Uuid::from_str(id.as_str());
+        assert!(parsed.is_ok());
+    }
+}
