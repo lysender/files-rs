0408fec350b97ac1be429815191dc7015efec41e
Lysender <lysender@darkstar-win>
2024-06-21 14:23:27 +0800

    fix: convert more

--- src/web/buckets/handlers.rs
+++ src/web/buckets/handlers.rs
@@ -1,8 +1,6 @@
 use axum::{
-    body::Body,
     extract::{Json, Path, State},
     http::StatusCode,
-    response::Response,
     Extension,
 };
 
@@ -11,14 +9,8 @@ use crate::{
         models::{Bucket, NewBucket, UpdateBucket},
         queries::buckets::{create_bucket, delete_bucket, get_bucket, list_buckets, update_bucket},
     },
-    web::{
-        response::{
-            create_error_response, create_response, create_success_response, to_error_response,
-            JsonResponse,
-        },
-        server::AppState,
-    },
-    Result,
+    web::{response::JsonResponse, server::AppState},
+    Error, Result,
 };
 
 pub async fn list_buckets_handler(State(state): State<AppState>) -> Result<JsonResponse> {
@@ -35,9 +27,7 @@ pub async fn create_bucket_handler(
     payload: Option<Json<NewBucket>>,
 ) -> Result<JsonResponse> {
     let Some(data) = payload else {
-        return Err(crate::Error::BadRequest(
-            "Invalid request payload".to_string(),
-        ));
+        return Err(Error::BadRequest("Invalid request payload".to_string()));
     };
     let bucket = create_bucket(&state.db_pool, &state.config.client_id, &data).await?;
     Ok(JsonResponse::with_status(
@@ -46,9 +36,9 @@ pub async fn create_bucket_handler(
     ))
 }
 
-pub async fn get_bucket_handler(Extension(bucket): Extension<Bucket>) -> Response<Body> {
+pub async fn get_bucket_handler(Extension(bucket): Extension<Bucket>) -> Result<JsonResponse> {
     // Extract bucket from the middleware extension
-    return create_success_response(serde_json::to_string(&bucket).unwrap());
+    Ok(JsonResponse::new(serde_json::to_string(&bucket).unwrap()))
 }
 
 pub async fn update_bucket_handler(
@@ -56,56 +46,39 @@ pub async fn update_bucket_handler(
     Extension(bucket): Extension<Bucket>,
     Path(bucket_id): Path<String>,
     payload: Option<Json<UpdateBucket>>,
-) -> Response<Body> {
+) -> Result<JsonResponse> {
     let Some(data) = payload else {
-        return create_error_response(
-            StatusCode::BAD_REQUEST,
-            "Invalid request payload".to_string(),
-            "Bad Request".to_string(),
-        );
+        return Err(Error::BadRequest("Invalid request payload".to_string()));
     };
-    let res = update_bucket(&state.db_pool, &bucket_id, &data).await;
-    match res {
-        Ok(updated) => {
-            if updated {
-                get_bucket_as_response(&state, &bucket_id).await
-            } else {
-                // Just send back the original bucket
-                create_success_response(serde_json::to_string(&bucket).unwrap())
-            }
-        }
-        Err(error) => to_error_response(error),
+    let updated = update_bucket(&state.db_pool, &bucket_id, &data).await?;
+
+    // Either return the updated bucket or the original one
+    match updated {
+        true => get_bucket_as_response(&state, &bucket_id).await,
+        false => Ok(JsonResponse::new(serde_json::to_string(&bucket).unwrap())),
     }
 }
 
-async fn get_bucket_as_response(state: &AppState, id: &str) -> Response<Body> {
+async fn get_bucket_as_response(state: &AppState, id: &str) -> Result<JsonResponse> {
     let res = get_bucket(&state.db_pool, id).await;
     let Ok(bucket_res) = res else {
-        return create_error_response(
-            StatusCode::INTERNAL_SERVER_ERROR,
-            "Error getting bucket".to_string(),
-            "Internal Server Error".to_string(),
-        );
+        return Err("Error getting bucket".into());
     };
 
     let Some(bucket) = bucket_res else {
-        return create_error_response(
-            StatusCode::INTERNAL_SERVER_ERROR,
-            "Error getting bucket, bucket not found".to_string(),
-            "Internal Server Error".to_string(),
-        );
+        return Err("Error getting bucket this time".into());
     };
 
-    create_success_response(serde_json::to_string(&bucket).unwrap())
+    Ok(JsonResponse::new(serde_json::to_string(&bucket).unwrap()))
 }
 
 pub async fn delete_bucket_handler(
     State(state): State<AppState>,
     Path(bucket_id): Path<String>,
-) -> Response<Body> {
-    let res = delete_bucket(&state.db_pool, &bucket_id).await;
-    match res {
-        Ok(_) => create_response(StatusCode::NO_CONTENT, "".to_string()),
-        Err(error) => to_error_response(error),
-    }
+) -> Result<JsonResponse> {
+    let _ = delete_bucket(&state.db_pool, &bucket_id).await?;
+    Ok(JsonResponse::with_status(
+        StatusCode::NO_CONTENT,
+        "".to_string(),
+    ))
 }
