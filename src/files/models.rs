7802c6da3e7b1c225685d2dd34562c507ed19d25
Lysender <lysender@darkstar-win>
2024-07-11 23:34:18 +0800

    fix: remove old model

--- src/files/models.rs
+++ src/files/models.rs
@@ -253,24 +253,3 @@ impl TryFrom<&str> for ImgVersion {
         }
     }
 }
-
-#[derive(Debug, Clone, Serialize)]
-pub struct FileDto {
-    pub name: String,
-    pub urls: FileUrls,
-}
-
-#[derive(Debug, Clone, Serialize)]
-pub struct FileUrls {
-    pub o: String,
-    pub s: String,
-}
-
-impl FileUrls {
-    pub fn new() -> Self {
-        Self {
-            o: "".to_string(),
-            s: "".to_string(),
-        }
-    }
-}
